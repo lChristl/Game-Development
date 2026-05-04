0ed18c33ab5875cd792537f21835f8f2fa9d70ca
Christian F. De Leon <chris20170322@gmail.com>
2026-05-04 19:04:05 +0800

    add the tasks to be done.

---
+++ Dungeon Crawler/game.py
@@ -0,0 +1,11 @@
+# simple dungeon text based game
+# where the player can input their name
+# have their own stats
+# you provide the player classes to choose from your own declared classes
+# attack system
+# defense and skill mechanism or additional mechanics
+# complete
+# gold and shop system
+# end game
+
+

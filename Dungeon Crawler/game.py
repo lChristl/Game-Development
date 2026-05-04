730cbf46ce3a71a6e5b9558fffd5f73624d32b60
Christian F. De Leon <chris20170322@gmail.com>
2026-05-04 23:01:31 +0800

    edited variables and values.

--- Dungeon Crawler/game.py
+++ Dungeon Crawler/game.py
@@ -24,24 +24,32 @@ print("*"*30)
 
 # Class List using dictionary
 player_jobs = {
-    "Paladin" : "Sword and shield",
-    "Ranger" : "Bow"
+    "Paladin" : {
+        "hp": 1,
+        "atk": 1,
+        "def": 1
+    }, 
+    "Ranger" : {
+        "hp": 1,
+        "atk": 1,
+        "def": 1
+    }, 
 }
 
 # sample_key = player_jobs.get("Paladin")
 # print(sample_key)
 
 monsters = [
-    ("Gob", 5, 10),
-    ("Orc", 10, 50),
-    ("Dragon", 1000, 10000)
+    ("Gob", 10, 2),
+    ("Orc", 50, 10),
+    ("Dragon", 10000, 1000)
 ]
 
 def spawn_monster (monsters):
     monster = random.choice(monsters)
     return [monster [0], monster [1], monster [2]]
 
-# monster_name, monster_atk, monster_hp = spawn_monster(monsters)
+# monster_name, monster_hp, monster_atk = spawn_monster(monsters)
 
 def start_game():
     print("Starting Game...")
@@ -57,9 +65,9 @@ def select_job():
     print("2. Ranger")
 
     while True:
-        choice = input("Enter choice: ")
+        job_choice = input("Enter choice: ")
 
-        match choice:
+        match job_choice:
             case "1":
                 return "Paladin"
             case "2":
@@ -69,21 +77,19 @@ def select_job():
 
 while True: #main menu choice
 
-    choice = input("What would you like to do?: ")
+    menu_choice = input("What would you like to do?: ")
     
-    match choice:
+    match menu_choice:
 
         case "1":
             start_game()
             
         case "2":
             print("Support the dev?\nGive a coffee.")
-
+        
         case "3":
             print("Exiting the game")
             break
         
         case _:
             print("Invalid choice")
-
-# print(f"You have encountered {monster_name}")
